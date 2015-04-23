43c5db1e3621b1d99e2ab56505732da27a5f463c
erica cha & chitra <student@epicodus.com>
2015-04-23 15:01:17 -0700

    add likes and order by likes functionality.

--- controllers/CommentsController.js
+++ controllers/CommentsController.js
@@ -1,6 +1,6 @@
 newsArticle.controller('CommentsCtrl', function CommentsCtrl($scope, $stateParams, ArticlesFactory, UtilitiesFactory) {
-  $scope.article = UtilitiesFactory.findById(ArticlesFactory.articles, $stateParams.articleId)
-  $scope.addComment = function() {
+  $scope.article = UtilitiesFactory.findById(ArticlesFactory.articles, $stateParams.articleId);
+    $scope.addComment = function() {
     $scope.article.comments.push({ name: $scope.commentor, text:$scope.commentText});
     $scope.commentor = null;
     $scope.commentText = null;
